aea33b886bbb18b014cec70ed610e7ca74459bd0
Photon Cat <lkstc112233@gmail.com>
2018-09-02 16:27:14 -0400

    Added basic button.

--- Button.ts
+++ Button.ts
@@ -3,7 +3,34 @@ import { getLoadedImage } from "./Images";
 
 export class Button {
     position: Point = new Point();
+    width: number = 30;
+    height: number = 30;
+    private captured: boolean = false;
+    onclick?: () => void;
+
+    get capturing(): boolean {
+        return this.captured;
+    }
+
+    mousedown(point: Point) {
+        if (point.x > this.position.x && point.y > this.position.y &&
+            point.x - this.position.x < this.width && point.y - this.position.y < this.height) {
+            this.captured = true;
+        }
+    }
+
+    mouseup(point: Point) {
+        if (this.captured && point.x > this.position.x && point.y > this.position.y &&
+            point.x - this.position.x < this.width && point.y - this.position.y < this.height) {
+            console.log("clicked");
+            if (this.onclick) {
+                this.onclick();
+            }
+        }
+        this.captured = false;
+    }
+
     draw(context: CanvasRenderingContext2D) {
-        context.drawImage(getLoadedImage('RESET_BUTTON'), this.position.x, this.position.y);
+        context.drawImage(getLoadedImage('RESET_BUTTON'), this.position.x, this.position.y, this.width, this.height);
     }
 }
--- main.ts
+++ main.ts
@@ -2,12 +2,15 @@ import { Point } from './xyTuple';
 import { Controller } from './Controller';
 import { loadedImageSum, totalImageSum, loadAll } from './Images';
 import { LevelB1 } from './Levels';
+import { Button } from './Button';
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const context = canvas.getContext('2d')!;
 
 const game = LevelB1.build();
 game.begin();
+
+const reset = new Button();
 
 function loadLoop() {
     if (loadedImageSum != totalImageSum) {
@@ -32,9 +35,13 @@ function gameLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     game.update();
     game.draw(context);
+    reset.draw(context);
 }
 
 canvas.addEventListener("touchmove", (ev) => {
+    if (reset.capturing) {
+        return;
+    }
     var rect = canvas.getBoundingClientRect();
     var x = ev.touches[0].clientX - rect.left;
     var y = ev.touches[0].clientY - rect.top;
@@ -47,17 +54,31 @@ canvas.addEventListener("touchstart", (ev) => {
         var rect = canvas.getBoundingClientRect();
         var x = ev.touches[0].clientX - rect.left;
         var y = ev.touches[0].clientY - rect.top;
+        reset.mousedown(new Point(x, y));
+        if (reset.capturing) {
+            return;
+        }
         game.touchBegin(new Point(x, y));
     }
 });
 
 canvas.addEventListener("touchend", (ev) => {
     if (ev.touches.length == 0) {
+        if (reset.capturing) {
+            var rect = canvas.getBoundingClientRect();
+            var x = ev.changedTouches[0].clientX - rect.left;
+            var y = ev.changedTouches[0].clientY - rect.top;
+            reset.mouseup(new Point(x, y));
+            return;
+        }
         game.touchEnd();
     }
 });
 
 canvas.addEventListener("mousemove", (ev) => {
+    if (reset.capturing) {
+        return;
+    }
     var rect = canvas.getBoundingClientRect();
     var x = ev.clientX - rect.left;
     var y = ev.clientY - rect.top;
@@ -69,12 +90,23 @@ canvas.addEventListener("mousedown", (ev) => {
         var rect = canvas.getBoundingClientRect();
         var x = ev.clientX - rect.left;
         var y = ev.clientY - rect.top;
+        reset.mousedown(new Point(x, y));
+        if (reset.capturing) {
+            return;
+        }
         game.touchBegin(new Point(x, y));
     }
 });
 
 canvas.addEventListener("mouseup", (ev) => {
     if (ev.button == 0) {
+        if (reset.capturing) {
+            var rect = canvas.getBoundingClientRect();
+            var x = ev.clientX - rect.left;
+            var y = ev.clientY - rect.top;
+            reset.mouseup(new Point(x, y));
+            return;
+        }
         game.touchEnd();
     }
 });
