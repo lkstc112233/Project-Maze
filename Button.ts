834b23c38425cb71fd8551c2646a80726a04cf80
Photon Cat <lkstc112233@gmail.com>
2018-09-03 16:12:22 -0400

    Added a handler handling multiple buttons

--- Button.ts
+++ Button.ts
@@ -33,3 +33,42 @@ export class Button {
         context.drawImage(getLoadedImage('RESET_BUTTON'), this.position.x, this.position.y, this.width, this.height);
     }
 }
+
+export class ButtonSet {
+    private buttons: Button[] = [];
+    add(button: Button) {
+        this.buttons.push(button);
+    }
+
+    clear() {
+        this.buttons = [];
+    }
+
+    // Returns true if the message is consumed by any of the button.
+    mousedown(point: Point): boolean {
+        var handled = false;
+        this.buttons.map((element) => {
+            if (handled) {
+                return;
+            }
+            element.mousedown(point);
+            if (element.capturing){
+                handled = true;
+            }
+        });
+        return handled;
+    }
+
+    mousemove(point: Point): boolean {
+        return this.buttons.reduce((handled, element) => handled || element.capturing, false);
+    }
+
+    mouseup(point: Point) {
+        this.buttons.map((element) => element.mouseup(point));
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        this.buttons.map((element) => element.draw(context));
+    }
+
+}
