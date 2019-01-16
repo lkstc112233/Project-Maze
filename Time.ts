a42281ffa37d3ef66255910cf2bd018989d26d3e
Photon Cat <lkstc112233@gmail.com>
2019-01-15 22:15:44 -0500

    Added drawing for the slider.

--- Time.ts
+++ Time.ts
@@ -3,6 +3,7 @@ import {Sprite} from './Scene'
 export class TimeSlider implements Sprite {
   current: number = 0;
   constructor(
+      private readonly width: number, private readonly y: number,
       private readonly timelimit: number) {}
 
   reset() {
@@ -20,5 +21,21 @@ export class TimeSlider implements Sprite {
     var rate: number = this.current / this.timelimit;
     rate = Math.min(rate, 1);
 
+    context.save();
+
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(0, this.y);
+    context.lineTo(this.width * rate, this.y);
+    context.strokeStyle = 'red';
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(this.width * rate, this.y);
+    context.lineTo(this.width, this.y);
+    context.strokeStyle = 'gray';
+    context.stroke();
+
+    context.restore();
   }
 }
