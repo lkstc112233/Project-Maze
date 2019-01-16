bd65eab5c4984a6da9b3008c97d8b8a7a3090f07
Photon Cat <lkstc112233@gmail.com>
2019-01-15 22:10:37 -0500

    Added time limits to the slider

--- Time.ts
+++ Time.ts
@@ -1,6 +1,14 @@
 import {Sprite} from './Scene'
 
 export class TimeSlider implements Sprite {
+  current: number = 0;
+  constructor(
+      private readonly timelimit: number) {}
+
+  reset() {
+    this.current = 0;
+  }
+
   get z(): number {
     return 0;
   }
