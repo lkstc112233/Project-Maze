a37888fdc9114669c06ea47b10179048ade957e8
Photon Cat <lkstc112233@gmail.com>
2019-01-17 23:13:59 -0500

    Implemented level1

--- Levels.ts
+++ Levels.ts
@@ -13,3 +13,16 @@ LevelB1.chestInitial = new Point(400, 350);
 LevelB1.obstacles = [
   new Obstacle(50, new Point(400, 100)), new Obstacle(200, new Point(100, 400))
 ];
+
+export const Level1 = new GameBuilder();
+Level1.left = 100;
+Level1.width = 400;
+Level1.top = 100;
+Level1.height = 400;
+Level1.timelimit = 100;
+Level1.keyInitial = new Point(200, 200);
+Level1.playerInitial = new Point(200, 50);
+Level1.chestInitial = new Point(200, 350);
+Level1.obstacles = [
+  new Obstacle(125, new Point(50, 200)), new Obstacle(125, new Point(350, 200))
+];
